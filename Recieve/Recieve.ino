b71959e922e25b74966fd039ad19d809e2e03934
Cameron H <enderman9523@gmail.com>
2024-03-12 09:44:37 -0600

    Comments to Recieve code

--- Recieve/Recieve.ino
+++ Recieve/Recieve.ino
@@ -2,11 +2,13 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
-const byte address[6] = "64479";
-RF24 radio(9, 8);
+const byte address[6] = "64479"; // Set address to 64479 (Team's unique signal code)
+RF24 radio(9, 8); // initialize radio on pins 9 and 8
 
 struct DataPacket {
-
+/*
+  Data Structure for recieving the input variables to the robot, called using inputdata
+*/
 int JoyLY = 0; // Left Joystick Y value
 int JoyLX = 0; // Left Joystick X value
 int JoyRY = 0; // Right Joystick Y value
@@ -18,23 +20,35 @@ bool B1T = 0; // Button 1 Toggle State BOOL
 bool B2T = 0; // Button 2 Toggle State BOOL
 
 };
+
 DataPacket inputdata;
+
 void setup() {
+  // Radio Setup
   radio.begin();
   radio.setPALevel(RF24_PA_LOW);
   radio.openReadingPipe(0, address);
   radio.startListening();
+
+  // Open serial port
   Serial.begin(9600);
 }
 
 void loop() {
-if (radio.available()) {//if a signal is available
-    radio.read(&inputdata, sizeof(DataPacket));
+
+if (radio.available()) { //if a signal is available
+    radio.read(&inputdata, sizeof(DataPacket)); // Read incoming data and store in datapacket datastructure
   }
+  
+  // print input variables read from the reciever
   SerialPrint();
+
+  // wait 50 ms to avoid too many updates happening too quickly
   delay(50);
 }
 
+
+// Function to directly print the data in the Datapacket Datastructure
 void SerialPrint() {
   Serial.print(inputdata.JoyLY);
   Serial.print("  ");
