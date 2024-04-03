5e74a1ecab4dc1ab4e415db467dbd0d97adcfa31
owenjeffcoat <165060579+owenjeffcoat@users.noreply.github.com>
2024-04-02 22:27:04 -0600

    Update Recieve.ino

--- Recieve/Recieve.ino
+++ Recieve/Recieve.ino
@@ -220,7 +220,7 @@ void dc_motor_control(int joystick_value, bool state1, bool state2, const int di
 // <DONE> DC motor control slow mode
 void dc_motor_control_slow(int joystick_value, bool state1, bool state2, const int dir1_pin, const int dir2_pin, const int motor_pin) {
   if (joystick_value < 400) {   //Forward
-    int spin_speed = map(joystick_value, 450, 0, 0, 64);   //Note: Joysticks are upside-down in the housing
+    int spin_speed = map(joystick_value, 450, 0, 0, 128);   //Note: Joysticks are upside-down in the housing
     state1 = HIGH;
     state2 = LOW;
     digitalWrite(dir1_pin, state1);
@@ -228,7 +228,7 @@ void dc_motor_control_slow(int joystick_value, bool state1, bool state2, const i
     analogWrite(motor_pin, spin_speed);
   }
   else if (joystick_value > 600) {    //Reverse
-    int spin_speed = map(joystick_value, 550, 1023, 0, 64);  //Note: Joysticks are upside-down in the housing
+    int spin_speed = map(joystick_value, 550, 1023, 0, 128);  //Note: Joysticks are upside-down in the housing
     state1 = LOW;
     state2 = HIGH;
     digitalWrite(dir1_pin, state1);
